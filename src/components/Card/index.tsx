47558b6b73e390534716662bdc87506894f2ff8f
daltheb <vtarcanjo@hotmail.com>
2022-04-01 20:43:54 -0300

    Style in ScrollReveal

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -13,20 +13,29 @@ type CardProps = {
 
 function Card({ date, isFavorite, title, paragraph, onFavorite }: CardProps) {
   return (
-    <S.Card>
-      <header>
-        <ScrollReveal delay={400}>
-          <span>{date}</span>
+    <ScrollReveal
+      delay={200}
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex'
+      }}
+    >
+      <S.Card>
+        <header>
+          <ScrollReveal delay={400}>
+            <span>{date}</span>
+          </ScrollReveal>
+          <FavoriteButton isFavorite={isFavorite} onClick={onFavorite} />
+        </header>
+        <ScrollReveal delay={500}>
+          <h2>{title}</h2>
         </ScrollReveal>
-        <FavoriteButton isFavorite={isFavorite} onClick={onFavorite} />
-      </header>
-      <ScrollReveal delay={500}>
-        <h2>{title}</h2>
-      </ScrollReveal>
-      <ScrollReveal delay={600}>
-        <p> {paragraph} </p>
-      </ScrollReveal>
-    </S.Card>
+        <ScrollReveal delay={600}>
+          <p> {paragraph} </p>
+        </ScrollReveal>
+      </S.Card>
+    </ScrollReveal>
   )
 }
 export default Card
